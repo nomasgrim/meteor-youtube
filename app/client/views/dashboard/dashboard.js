d151c5d1c050531c5d3da228d86b671c3b6db478
cbare1208 <cbarendsen@vml.com>
2015-02-20 12:06:16 -0600

    updating

--- app/client/views/dashboard/dashboard.js
+++ app/client/views/dashboard/dashboard.js
@@ -1,21 +1,2 @@
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.dashboard.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
-  });
-
-  Template.dashboard.events({
-    'click .video-pod' : function(event) {
-        var view = event.target;
-        console.log(this.text);
-        //Template.onClientLoad();
-        search();
-        return false;
-    }
-  });
-
 }
